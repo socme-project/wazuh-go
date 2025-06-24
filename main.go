06c77012be70f24c09024f5fe7a345481b8501c5
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-06-24 11:11:50 +0200

    remove comment
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 )
 
-// curl -u <USER>:<PASSWORD> -k -X POST "https://<HOST_IP>:55000/security/user/authenticate"
 func (w *WazuhAPI) RefreshToken() (err error) {
 	resp, err := w.Call("/security/user/authenticate", "POST+BA", "")
 	if err != nil {
